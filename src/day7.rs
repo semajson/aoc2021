93faad6a60afc6f03c0e4b5e637b2ddc49484747
James Sandy <james.sandy@metaswitch.com>
2021-12-12 14:05:16 +0000

    working, kinda...

--- src/day7.rs
+++ src/day7.rs
@@ -2,6 +2,7 @@ use std::num;
 
 pub trait CrabT {
     fn fuel_to_get_to_pos(&self, pos: i64) -> i64;
+    fn get_pos(&self) -> i64;
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -15,6 +16,9 @@ impl Crab1 {
 impl CrabT for Crab1 {
     fn fuel_to_get_to_pos(&self, pos: i64) -> i64 {
         (pos - self.0).abs()
+    }
+    fn get_pos(&self) -> i64 {
+        self.0
     }
 }
 
@@ -33,35 +37,51 @@ impl CrabT for Crab2 {
         // use arth sum 1....horz_diff
         horz_diff * (1 + horz_diff) / 2
     }
+    fn get_pos(&self) -> i64 {
+        self.0
+    }
 }
 
-fn parse_input_lines(
-    input_lines: &[String],
-    part1: bool,
-) -> Result<Vec<Crab1>, num::ParseIntError> {
-    let mut parsed_data = Vec::new();
+// fn parse_input_lines(
+//     input_lines: &[String],
+//     part1: bool,
+// ) -> Result<Vec<impl CrabT>, num::ParseIntError> {
+//     let input_lines = input_lines[0].split(',').collect::<Vec<&str>>();
 
+//     if part1 {
+//         let mut parsed_data = Vec::new();
+//         for line in input_lines {
+//             parsed_data.push(Crab1::new(line)?);
+//         }
+//         return Ok(parsed_data);
+//     } else {
+//         let mut parsed_data = Vec::new();
+//         for line in input_lines {
+//             parsed_data.push(Crab2::new(line)?);
+//         }
+//         return Ok(parsed_data);
+//     }
+// }
+fn parse_input_lines_part1(input_lines: &[String]) -> Result<Vec<impl CrabT>, num::ParseIntError> {
     let input_lines = input_lines[0].split(',').collect::<Vec<&str>>();
 
+    let mut parsed_data = Vec::new();
     for line in input_lines {
         parsed_data.push(Crab1::new(line)?);
     }
-    Ok(parsed_data)
+    return Ok(parsed_data);
 }
+fn parse_input_lines_part2(input_lines: &[String]) -> Result<Vec<impl CrabT>, num::ParseIntError> {
+    let input_lines = input_lines[0].split(',').collect::<Vec<&str>>();
 
-pub fn tot_fuel_at_pos_part_2(crabs: &Vec<Crab1>, pos: i64) -> i64 {
-    let mut tot_fuel = 0;
-    for crab in crabs {
-        let horz_diff = (pos - crab.0).abs();
-
-        // use arth sum 1....horz_diff
-        tot_fuel += horz_diff * (1 + horz_diff) / 2;
+    let mut parsed_data = Vec::new();
+    for line in input_lines {
+        parsed_data.push(Crab2::new(line)?);
     }
-
-    tot_fuel
+    return Ok(parsed_data);
 }
 
-pub fn tot_fuel_at_pos(crabs: &Vec<Crab1>, pos: i64) -> i64 {
+pub fn tot_fuel_at_pos(crabs: &Vec<impl CrabT>, pos: i64) -> i64 {
     let mut tot_fuel = 0;
     for crab in crabs {
         tot_fuel += crab.fuel_to_get_to_pos(pos);
@@ -70,11 +90,12 @@ pub fn tot_fuel_at_pos(crabs: &Vec<Crab1>, pos: i64) -> i64 {
     tot_fuel
 }
 
-pub fn part_1(parsed_data: &Vec<Crab1>) -> i64 {
-    let crabs = (*parsed_data).clone();
+pub fn calc_optimal_pos(parsed_data: &Vec<impl CrabT>) -> i64 {
+    // let crabs = (*parsed_data).clone();
+    let crabs = parsed_data;
 
     // Find the new one
-    let largest_post = crabs.iter().map(|crab| crab.0).max().unwrap();
+    let largest_post = crabs.iter().map(|crab| crab.get_pos()).max().unwrap();
 
     let mut prev_value = tot_fuel_at_pos(&crabs, 0);
     for i in 1..=largest_post {
@@ -85,38 +106,17 @@ pub fn part_1(parsed_data: &Vec<Crab1>) -> i64 {
         prev_value = cur_value;
     }
     return prev_value;
-
-    // Sum all the fuel
-}
-
-pub fn part_2(parsed_data: &Vec<Crab1>) -> i64 {
-    let crabs = (*parsed_data).clone();
-
-    // Find the new one
-    let largest_post = crabs.iter().map(|crab| crab.0).max().unwrap();
-
-    let mut prev_value = tot_fuel_at_pos_part_2(&crabs, 0);
-    for i in 1..=largest_post {
-        let cur_value = tot_fuel_at_pos_part_2(&crabs, i);
-        if cur_value > prev_value {
-            return prev_value;
-        }
-        prev_value = cur_value;
-    }
-    return prev_value;
-
-    // Sum all the fuel
 }
 
 pub fn day7(input_lines: &[String]) -> (u64, u64) {
-    let parsed_test_data = parse_input_lines(input_lines, true).unwrap_or_else(|err| {
+    let parsed_data = parse_input_lines_part1(input_lines).unwrap_or_else(|err| {
         panic!("Got error {} when trying to parse the input lines", err);
     });
-    let part1 = part_1(&parsed_test_data) as u64;
+    let part1 = calc_optimal_pos(&parsed_data) as u64;
 
-    let parsed_test_data = parse_input_lines(input_lines, false).unwrap_or_else(|err| {
+    let parsed_data = parse_input_lines_part2(input_lines).unwrap_or_else(|err| {
         panic!("Got error {} when trying to parse the input lines", err);
     });
-    let part2 = part_2(&parsed_test_data) as u64;
+    let part2 = calc_optimal_pos(&parsed_data) as u64;
     (part1, part2)
 }

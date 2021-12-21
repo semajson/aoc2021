d48595120b9d47c0185c701c342cb13c7c498bbc
James Sandy <james.sandy@metaswitch.com>
2021-12-21 18:43:41 +0000

    clean cod

--- src/day10.rs
+++ src/day10.rs
@@ -1,13 +1,11 @@
 use std::collections::HashMap;
 use std::num;
 
-// #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 #[derive(Debug, Clone)]
 pub struct Line {
     data: Vec<String>,
-    is_incomplete: bool,
-    is_corrupted: bool,
     first_illegal_char: Option<String>,
+    chars_to_make_complete: Option<Vec<String>>,
 }
 impl Line {
     pub fn new(input_line: &String) -> Line {
@@ -20,9 +18,8 @@ impl Line {
 
         Line {
             data: line,
-            is_incomplete: false,
-            is_corrupted: false,
             first_illegal_char: None,
+            chars_to_make_complete: None,
         }
     }
 
@@ -36,7 +33,7 @@ impl Line {
             if bracket_matches.contains_key(bracket as &str) {
                 bracket_stack.push(bracket);
             }
-            // i don't like this, clean later..
+            // i don't like this, but can't think of a better way...
             else if (!bracket_stack.is_empty())
                 && (bracket
                     == bracket_matches
@@ -45,14 +42,26 @@ impl Line {
             {
                 bracket_stack.pop().unwrap();
             } else {
-                self.is_corrupted = true;
+                // Is corrupted
                 self.first_illegal_char = Some(bracket.clone());
                 return;
             }
         }
-        self.is_incomplete = true;
+
+        // Must be incomplete - work out chars needed to make complete
+        let mut chars_to_make_complete = vec![];
+        for open_bracket in bracket_stack.iter().rev() {
+            chars_to_make_complete.push(
+                bracket_matches
+                    .get(open_bracket as &str)
+                    .unwrap()
+                    .to_string(),
+            );
+        }
+        self.chars_to_make_complete = Some(chars_to_make_complete.clone());
     }
 }
+
 fn parse_input_lines(raw_input_lines: &[String]) -> Result<Vec<Line>, num::ParseIntError> {
     let input_lines = raw_input_lines.iter().collect::<Vec<&String>>();
 
@@ -69,20 +78,43 @@ pub fn part_1(parsed_data: &Vec<Line>) -> i64 {
         .iter_mut()
         .for_each(|line| line.workout_is_corrupted_or_incomplete());
 
-    let bracket_matches: HashMap<&str, usize> =
+    // Work out the corrupted score
+    let corrupted_bracket_scores: HashMap<&str, usize> =
         HashMap::from([(")", 3), ("]", 57), ("}", 1197), (">", 25137)]);
     let mut sum = 0;
     for line in parsed_data.into_iter() {
         if let Some(illegal_char) = line.first_illegal_char {
-            sum += bracket_matches.get(&illegal_char as &str).unwrap();
+            sum += corrupted_bracket_scores.get(&illegal_char as &str).unwrap();
         }
     }
     sum as i64
 }
 
 pub fn part_2(parsed_data: &Vec<Line>) -> i64 {
-    println!("d");
-    0
+    let mut parsed_data = parsed_data.clone();
+    parsed_data
+        .iter_mut()
+        .for_each(|line| line.workout_is_corrupted_or_incomplete());
+
+    // Work out the incomplete score
+    let incomplete_bracket_scores: HashMap<&str, usize> =
+        HashMap::from([(")", 1), ("]", 2), ("}", 3), (">", 4)]);
+    let mut scores = vec![];
+    for line in parsed_data.into_iter() {
+        if let Some(chars_to_make_complete) = line.chars_to_make_complete {
+            let mut score = 0;
+            for char in chars_to_make_complete.iter() {
+                score *= 5;
+                score += incomplete_bracket_scores.get(char as &str).unwrap();
+            }
+            scores.push(score);
+        }
+    }
+    scores.sort();
+
+    // Return the middle score
+    let middle_index = ((scores.len() + 1) / 2) - 1;
+    scores[middle_index] as i64
 }
 
 pub fn day10(input_lines: &[String]) -> (u64, u64) {

b5e004675d370b77a4fde0194c1b3fedb85060a7
James Sandy <james.sandy@metaswitch.com>
2022-01-06 22:21:05 +0000

    part 0

---
+++ src/day17.rs
@@ -0,0 +1,149 @@
+use std::{collections::btree_set::Intersection, num};
+#[derive(Debug, Clone)]
+pub struct Coord {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug, Clone)]
+pub struct Velocity {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug, Clone)]
+pub struct TargetArea {
+    top_left: Coord,
+    bottom_right: Coord,
+}
+impl TargetArea {
+    pub fn new(line: &str) -> Result<TargetArea, num::ParseIntError> {
+        // target area: x=20..30, y=-10..-5
+        let line = line.replace("target area: ", "");
+        let line = line.replace("x=", "");
+        let line = line.replace("y=", "");
+        // 20..30, 0..-5
+
+        let raw_coords = line.split(", ").collect::<Vec<&str>>();
+        let raw_coords = raw_coords
+            .iter()
+            .map(|z| z.split("..").collect::<Vec<&str>>())
+            .collect::<Vec<Vec<&str>>>();
+        // [[20, 30], [0, -5]]
+
+        let top_left_x = raw_coords[0][0].parse::<i32>()?;
+        let top_left_y = raw_coords[1][1].parse::<i32>()?;
+
+        let bottom_right_x = raw_coords[0][1].parse::<i32>()?;
+        let bottom_right_y = raw_coords[1][0].parse::<i32>()?;
+
+        let top_left = Coord {
+            x: top_left_x,
+            y: top_left_y,
+        };
+        let bottom_right = Coord {
+            x: bottom_right_x,
+            y: bottom_right_y,
+        };
+        Ok(TargetArea {
+            top_left,
+            bottom_right,
+        })
+    }
+}
+
+pub struct Projectile {
+    position: Coord,
+    velocity: Velocity,
+    max_y: i32,
+}
+impl Projectile {
+    pub fn new(position: Coord, velocity: Velocity) -> Projectile {
+        Projectile {
+            max_y: position.y.clone(),
+            position,
+            velocity,
+        }
+    }
+
+    pub fn do_step(&mut self) {
+        // Find new position
+        self.position.x += self.velocity.x;
+        self.position.y += self.velocity.y;
+
+        if self.position.y > self.max_y {
+            self.max_y = self.position.y;
+        }
+
+        // Find new x velocity
+        let mut x_drag_change = 0;
+        if self.velocity.x > 0 {
+            x_drag_change = -1;
+        } else if self.velocity.x < 0 {
+            x_drag_change = 1;
+        }
+
+        self.velocity.x += x_drag_change;
+
+        // Find new y velocity
+        let y_gravity_change = -1;
+        self.velocity.y += y_gravity_change;
+    }
+
+    pub fn does_projectile_hit_area_after_fire(&mut self, area: &TargetArea) -> bool {
+        loop {
+            if self.in_area(&area) {
+                return true;
+            } else if self.cant_hit_area(&area) {
+                return false;
+            }
+            self.do_step();
+        }
+    }
+    pub fn in_area(&self, area: &TargetArea) -> bool {
+        if (self.position.x >= area.top_left.x)
+            && (self.position.x <= area.bottom_right.x)
+            && (self.position.y <= area.top_left.y)
+            && (self.position.y >= area.bottom_right.y)
+        {
+            return true;
+        }
+
+        false
+    }
+    pub fn cant_hit_area(&self, area: &TargetArea) -> bool {
+        if (self.position.x > area.bottom_right.x) && (self.position.y < area.bottom_right.y) {
+            return true;
+        }
+        false
+    }
+}
+
+fn parse_input_lines(raw_input_lines: &[String]) -> Result<TargetArea, num::ParseIntError> {
+    let input_lines = raw_input_lines.iter().collect::<Vec<&String>>();
+    assert!(input_lines.len() == 1);
+    TargetArea::new(input_lines[0])
+}
+
+pub fn part_1(target_area: &TargetArea) -> i64 {
+    let mut current_max_y = 0;
+
+    let mut projectile = Projectile::new(Coord { x: 0, y: 0 }, Velocity { x: 7, y: 2 });
+    if projectile.does_projectile_hit_area_after_fire(target_area) {
+        if projectile.max_y > current_max_y {
+            current_max_y = projectile.max_y;
+        }
+    }
+    current_max_y as i64
+}
+
+pub fn part_2(encoded_data: &TargetArea) -> i64 {
+    0
+}
+
+pub fn day17(input_lines: &[String]) -> (u64, u64) {
+    let encoded_data = parse_input_lines(input_lines).unwrap_or_else(|err| {
+        panic!("Got error : {} , when trying to parse the input lines", err);
+    });
+    (part_1(&encoded_data) as u64, part_2(&encoded_data) as u64)
+}
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod day13;
 mod day14;
 mod day15;
 mod day16;
+mod day17;
 mod day2;
 mod day3;
 mod day4;
@@ -20,7 +21,7 @@ mod day9;
 mod utils;
 
 type DayFunction = fn(&[String]) -> (u64, u64);
-static DAY_FUNCTIONS: [DayFunction; 16] = [
+static DAY_FUNCTIONS: [DayFunction; 17] = [
     day1::day1,
     day2::day2,
     day3::day3,
@@ -37,6 +38,7 @@ static DAY_FUNCTIONS: [DayFunction; 16] = [
     day14::day14,
     day15::day15,
     day16::day16,
+    day17::day17,
 ];
 
 fn solve_day(day: usize) {

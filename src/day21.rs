a462ea6d2a1c70865585ceb298c4c142ab837912
semajson <jamessandy95@gmail.com>
2022-09-04 22:06:44 +0100

    part 1 work

--- src/day21.rs
+++ src/day21.rs
@@ -3,23 +3,59 @@ use std::num;
 
 #[derive(Debug, Clone)]
 pub struct Player {
-    position: usize,
-    score: usize,
+    position: isize,
+    score: isize,
 }
 impl Player {
     pub fn new(input_line: &String) -> Player {
         let input_line = input_line.split(": ");
-        let position = input_line.last().unwrap().parse::<usize>().unwrap();
+        let position = input_line.last().unwrap().parse::<isize>().unwrap();
 
         Player { position, score: 0 }
     }
 
-    // pub fn count_lit_pixles(&self) -> usize {
-    //     self.map
-    //         .keys()
-    //         .filter(|x| *self.map.get(*x).unwrap() == '#')
-    //         .count()
-    // }
+    pub fn do_turn(&mut self, rolls: &mut isize, dice_num: &mut isize) {
+        if *dice_num > 97 {
+            println!("in here");
+        }
+        let mut dice_total = 0;
+        for _ in 0..3 {
+            dice_total += *dice_num;
+            *dice_num = (*dice_num % 100) + 1;
+            *rolls += 1;
+        }
+
+        self.position = self.position + dice_total;
+        if self.position > 10 {
+            if (self.position % 10) == 0 {
+                self.position = 10;
+            } else {
+                self.position = (self.position % 10);
+            }
+        }
+        self.score += self.position;
+    }
+    pub fn do_dirac_turn(&mut self, rolls: &mut isize, dice_num: &mut isize) {
+        if *dice_num > 97 {
+            println!("in here");
+        }
+        let mut dice_total = 0;
+        for _ in 0..3 {
+            dice_total += *dice_num;
+            *dice_num = (*dice_num % 100) + 1;
+            *rolls += 1;
+        }
+
+        self.position = self.position + dice_total;
+        if self.position > 10 {
+            if (self.position % 10) == 0 {
+                self.position = 10;
+            } else {
+                self.position = (self.position % 10);
+            }
+        }
+        self.score += self.position;
+    }
 }
 
 fn parse_input_lines(raw_input_lines: &[String]) -> Result<(Player, Player), num::ParseIntError> {
@@ -32,21 +68,73 @@ fn parse_input_lines(raw_input_lines: &[String]) -> Result<(Player, Player), num
 }
 
 pub fn part_1((player_1, player_2): (&Player, &Player)) -> i64 {
-    let player_1 = player_1.clone();
-    let player_2 = player_2.clone();
+    let mut player_1 = player_1.clone();
+    let mut player_2 = player_2.clone();
 
     // image.enhance(enhance_algo);
     // image.enhance(enhance_algo);
+    let mut rolls = 0 as isize;
+    let mut dice_num = 1 as isize;
+    let mut turn = 1 as isize;
+    loop {
+        match turn {
+            1 => {
+                player_1.do_turn(&mut rolls, &mut dice_num);
+                turn = 2;
+            }
+            2 => {
+                player_2.do_turn(&mut rolls, &mut dice_num);
+                turn = 1;
+            }
+            _ => panic!("invalid branch"),
+        }
 
-    // image.count_lit_pixles() as i64
-    player_1.position as i64
+        if (player_1.score >= 1000) {
+            println!("rolls is:{:}", rolls);
+            println!("player2_score is:{:}", player_2.score);
+            return (rolls * player_2.score) as i64;
+        } else if (player_2.score >= 1000) {
+            println!("rolls is:{:}", rolls);
+            println!("player1_score is:{:}", player_1.score);
+            return (rolls * player_2.score) as i64;
+        }
+    }
+
+    panic!("error");
 }
 
 pub fn part_2((player_1, player_2): (&Player, &Player)) -> i64 {
-    let player_1 = player_1.clone();
-    let player_2 = player_2.clone();
+    let mut player_1 = player_1.clone();
+    let mut player_2 = player_2.clone();
 
-    0
+    // image.enhance(enhance_algo);
+    // image.enhance(enhance_algo);
+    let mut rolls = 0 as isize;
+    let mut dice_num = 1 as isize;
+    let mut turn = 1 as isize;
+    loop {
+        match turn {
+            1 => {
+                player_1.do_dirac_turn(&mut rolls, &mut dice_num);
+                turn = 2;
+            }
+            2 => {
+                player_2.do_dirac_turn(&mut rolls, &mut dice_num);
+                turn = 1;
+            }
+            _ => panic!("invalid branch"),
+        }
+
+        if (player_1.score >= 1000) {
+            println!("rolls is:{:}", rolls);
+            println!("player2_score is:{:}", player_2.score);
+            return (rolls * player_2.score) as i64;
+        } else if (player_2.score >= 1000) {
+            println!("rolls is:{:}", rolls);
+            println!("player1_score is:{:}", player_1.score);
+            return (rolls * player_2.score) as i64;
+        }
+    }
 }
 
 pub fn day21(input_lines: &[String]) -> (u64, u64) {
@@ -58,4 +146,52 @@ pub fn day21(input_lines: &[String]) -> (u64, u64) {
         part_1((&player_1, &player_2)) as u64,
         part_2((&player_1, &player_2)) as u64,
     )
+}
+
+#[test]
+fn day21_part1_tests() {
+    let mut player_1 = Player {
+        position: 4,
+        score: 0,
+    };
+    let mut player_2 = Player {
+        position: 8,
+        score: 0,
+    };
+    let mut rolls = 1;
+    let mut dice_num = 1;
+    player_1.do_turn(&mut rolls, &mut dice_num);
+    assert!(player_1.score == 10);
+
+    player_2.do_turn(&mut rolls, &mut dice_num);
+    assert!(player_2.score == 3);
+
+    player_1.do_turn(&mut rolls, &mut dice_num);
+    assert!(player_1.score == 14);
+
+    player_2.do_turn(&mut rolls, &mut dice_num);
+    assert!(player_2.score == 9);
+
+    player_1.do_turn(&mut rolls, &mut dice_num);
+    assert!(player_1.score == 20);
+
+    player_2.do_turn(&mut rolls, &mut dice_num);
+    assert!(player_2.score == 16);
+
+    let mut player_1 = Player {
+        position: 4,
+        score: 990,
+    };
+    let mut player_2 = Player {
+        position: 6,
+        score: 742,
+    };
+    let mut rolls = 1;
+    let mut dice_num = 88;
+
+    player_2.do_turn(&mut rolls, &mut dice_num);
+    assert!(player_2.score == 745);
+
+    player_1.do_turn(&mut rolls, &mut dice_num);
+    assert!(player_1.score == 1000);
 }
